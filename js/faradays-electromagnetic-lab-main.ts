ba5efa17adb5e9366fdf9e6fe434821c212a929f
pixelzoom <cmalley@pixelzoom.com>
2023-10-27 18:48:40 -0600

    delete boilerplate comment

--- js/faradays-electromagnetic-lab-main.ts
+++ js/faradays-electromagnetic-lab-main.ts
@@ -13,8 +13,6 @@ import FaradaysElectromagneticLabScreen from './faradays-electromagnetic-lab/Far
 import FaradaysElectromagneticLabStrings from './FaradaysElectromagneticLabStrings.js';
 import './common/FaradaysElectromagneticLabQueryParameters.js';
 
-// Launch the sim. Beware that scenery Image nodes created outside simLauncher.launch() will have zero bounds
-// until the images are fully loaded. See https://github.com/phetsims/coulombs-law/issues/70#issuecomment-429037461
 simLauncher.launch( () => {
 
   const titleStringProperty = FaradaysElectromagneticLabStrings[ 'faradays-electromagnetic-lab' ].titleStringProperty;
