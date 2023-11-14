da62b971d280563c39475329b1b8d213872e7ca4
pixelzoom <cmalley@pixelzoom.com>
2023-11-14 12:55:47 -0700

    refine grabbing on EarthNode

--- js/common/view/EarthNode.ts
+++ js/common/view/EarthNode.ts
@@ -7,22 +7,30 @@
  */
 
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
-import { DragListener, Image, Node } from '../../../../scenery/js/imports.js';
+import { DragListener, Image, Node, Path } from '../../../../scenery/js/imports.js';
 import BarMagnet from '../model/BarMagnet.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
 import earth_png from '../../../images/earth_png.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import { Shape } from '../../../../kite/js/imports.js';
 
 export default class EarthNode extends Node {
 
   public constructor( barMagnet: BarMagnet, visibleProperty: TReadOnlyProperty<boolean>, tandem: Tandem ) {
 
     const earthImage = new Image( earth_png, {
-      scale: 0.6
+      scale: 0.6,
+      pickable: false // ... so earthPath determines where this Node can be grabbed.
+    } );
+
+    // ... so this Node can only be grabbed by the circular shape that matches earthImage.
+    const earthPath = new Path( Shape.circle( earthImage.width / 2 ), {
+      fill: 'transparent',
+      center: earthImage.center
     } );
 
     super( {
-      children: [ earthImage ],
+      children: [ earthPath, earthImage ],
       visibleProperty: visibleProperty,
       cursor: 'pointer',
       tagName: 'div',
