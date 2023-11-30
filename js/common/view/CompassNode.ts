7570ad38234c5dcfbcae13db9825aaadb8648e71
pixelzoom <cmalley@pixelzoom.com>
2023-11-29 21:41:15 -0700

    flesh out CompassNode

--- js/common/view/CompassNode.ts
+++ js/common/view/CompassNode.ts
@@ -1,5 +1,9 @@
 // Copyright 2023, University of Colorado Boulder
 
+//TODO dragBounds
+//TODO collision detection
+//TODO color profile
+
 /**
  * CompassNode is the visualization of a compass, whose orientation matches a magnet's B-field.
  *
@@ -9,22 +13,54 @@
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
 import Compass from '../model/Compass.js';
 import TReadOnlyProperty from '../../../../axon/js/TReadOnlyProperty.js';
+import Vector2 from '../../../../dot/js/Vector2.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
-import { Circle, DragListener, KeyboardDragListener, KeyboardDragListenerOptions, Node } from '../../../../scenery/js/imports.js';
+import { Circle, Color, DragListener, KeyboardDragListener, KeyboardDragListenerOptions, Node, Path } from '../../../../scenery/js/imports.js';
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import FELConstants from '../FELConstants.js';
+import CompassNeedleNode from './CompassNeedleNode.js';
+import { Shape } from '../../../../kite/js/imports.js';
+import Utils from '../../../../dot/js/Utils.js';
+
+const RING_RADIUS = 40;
+const RING_LINE_WIDTH = 10;
+const NEEDLE_LENGTH = 55;
+const NEEDLE_ANCHOR_RADIUS = 3;
+const INDICATOR_SPACING = 45; // degrees
+const INDICATOR_RADIUS = 3;
 
 export default class CompassNode extends Node {
 
   public constructor( compass: Compass, visibleProperty: TReadOnlyProperty<boolean>, tandem: Tandem ) {
 
-    const bodyNode = new Circle( 40, {
-      fill: 'white',
-      stroke: 'black'
+    const ringNode = new Circle( RING_RADIUS, {
+      stroke: Color.grayColor( 153 ),
+      lineWidth: RING_LINE_WIDTH,
+      boundsMethod: 'accurate' // so that stroke is included TODO not working as expected
+    } );
+
+    // Indicators at evenly-spaced increments around the ring.
+    let indicatorAngle = 0; // degrees
+    const indicatorsShape = new Shape();
+    while ( indicatorAngle < 360 ) {
+      const vector = Vector2.createPolar( RING_RADIUS, Utils.toRadians( indicatorAngle ) );
+      indicatorsShape.circle( vector, INDICATOR_RADIUS );
+      indicatorAngle += INDICATOR_SPACING;
+    }
+    const indicatorsNode = new Path( indicatorsShape, {
+      fill: 'black'
+    } );
+
+    const needleNode = new CompassNeedleNode( NEEDLE_LENGTH );
+    needleNode.center = ringNode.center;
+
+    const needleAnchorNode = new Circle( NEEDLE_ANCHOR_RADIUS, {
+      fill: 'black',
+      center: ringNode.center
     } );
 
     super( {
-      children: [ bodyNode ],
+      children: [ ringNode, indicatorsNode, needleNode, needleAnchorNode ],
       cursor: 'pointer',
       visibleProperty: visibleProperty,
       tagName: 'div', // for KeyboardDragListener
@@ -35,6 +71,9 @@ export default class CompassNode extends Node {
     compass.positionProperty.link( position => {
       this.center = position;
     } );
+
+    //TODO not working as expected with KinematicCompass
+    compass.rotationProperty.link( rotation => needleNode.rotateAround( needleNode.center, rotation ) );
 
     const dragListener = new DragListener( {
       positionProperty: compass.positionProperty,
