234d4cf77ff6ae759d188f739926c2d07882e5b4
pixelzoom <cmalley@pixelzoom.com>
2023-12-04 12:37:34 -0700

    remove MappedProperty from LoopRadiusControl

--- js/common/view/LoopRadiusControl.ts
+++ js/common/view/LoopRadiusControl.ts
@@ -16,9 +16,7 @@ import FELConstants from '../FELConstants.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
 import Utils from '../../../../dot/js/Utils.js';
 import { RichText } from '../../../../scenery/js/imports.js';
-import MappedProperty from '../../../../axon/js/MappedProperty.js';
-import NumberIO from '../../../../tandem/js/types/NumberIO.js';
-import Range from '../../../../dot/js/Range.js';
+import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 
 const SLIDER_STEP = 1;
 const TICK_LABEL_OPTIONS = {
@@ -29,37 +27,21 @@ export default class LoopRadiusControl extends NumberControl {
 
   public constructor( loopRadiusProperty: NumberProperty, tandem: Tandem ) {
 
-    // loopRadiusProperty is a unitless dimension, while this control is in %.
-    const percentProperty = new MappedProperty<number, number>( loopRadiusProperty, {
-      phetioValueType: NumberIO,
-      reentrant: true,
-      bidirectional: true,
-
-      // loopRadius => %
-      map: ( loopRadius: number ) => 100 * loopRadius / loopRadiusProperty.range.max,
-
-      // % => loopRadius
-      inverseMap: ( percent: number ) => ( percent / 100 ) * loopRadiusProperty.range.max
-    } );
-
-    const minPercent = Math.ceil( 100 * loopRadiusProperty.rangeProperty.value.min / loopRadiusProperty.rangeProperty.value.max );
-    const percentRange = new Range( minPercent, 100 );
-
     const majorTicks = [
 
       // 45%
       {
-        value: percentRange.min,
+        value: loopRadiusProperty.rangeProperty.value.min,
         label: new RichText( new PatternStringProperty( FaradaysElectromagneticLabStrings.pattern.valuePercentStringProperty, {
-          value: percentRange.min
+          value: Utils.roundToInterval( 100 * loopRadiusProperty.rangeProperty.value.min / loopRadiusProperty.rangeProperty.value.max, 1 )
         } ), TICK_LABEL_OPTIONS )
       },
 
       // 100%
       {
-        value: percentRange.max,
+        value: loopRadiusProperty.rangeProperty.value.max,
         label: new RichText( new PatternStringProperty( FaradaysElectromagneticLabStrings.pattern.valuePercentStringProperty, {
-          value: percentRange.max
+          value: 100
         } ), TICK_LABEL_OPTIONS )
       }
     ];
@@ -68,7 +50,9 @@ export default class LoopRadiusControl extends NumberControl {
       delta: SLIDER_STEP,
       numberDisplayOptions: {
         decimalPlaces: 0,
-        valuePattern: FaradaysElectromagneticLabStrings.pattern.valuePercentStringProperty
+        numberFormatter: loopRadius => StringUtils.fillIn( FaradaysElectromagneticLabStrings.pattern.valuePercentStringProperty, {
+          value: Utils.roundToInterval( 100 * loopRadius / loopRadiusProperty.rangeProperty.value.max, 1 )
+        } )
       },
       sliderOptions: {
         constrainValue: ( value: number ) => Utils.roundToInterval( value, SLIDER_STEP ),
@@ -82,8 +66,8 @@ export default class LoopRadiusControl extends NumberControl {
 
     super(
       FaradaysElectromagneticLabStrings.loopRadiusStringProperty,
-      percentProperty,
-      percentRange,
+      loopRadiusProperty,
+      loopRadiusProperty.rangeProperty.value,
       options
     );
   }
