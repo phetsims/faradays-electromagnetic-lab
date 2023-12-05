26b492b43e19ba5816e5ecb7e3ead5c4efcd0b9a
pixelzoom <cmalley@pixelzoom.com>
2023-12-04 18:18:45 -0700

    flesh out keyboard help

--- js/transformer/view/TransformerKeyboardHelpContent.ts
+++ js/transformer/view/TransformerKeyboardHelpContent.ts
@@ -1,5 +1,6 @@
 // Copyright 2023, University of Colorado Boulder
 
+//TODO same as BarMagnetKeyboardHelpContent?
 /**
  * TransformerKeyboardHelpContent is the keyboard help for the 'Transformer' screen.
  *
@@ -8,22 +9,26 @@
 
 import BasicActionsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/BasicActionsKeyboardHelpSection.js';
 import TwoColumnKeyboardHelpContent from '../../../../scenery-phet/js/keyboard/help/TwoColumnKeyboardHelpContent.js';
-import KeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/KeyboardHelpSection.js';
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
+import MoveDraggableItemsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/MoveDraggableItemsKeyboardHelpSection.js';
+import SliderControlsKeyboardHelpSection from '../../../../scenery-phet/js/keyboard/help/SliderControlsKeyboardHelpSection.js';
 
 export default class TransformerKeyboardHelpContent extends TwoColumnKeyboardHelpContent {
 
   public constructor() {
 
     // Sections in the left column.
-    const leftSections: KeyboardHelpSection[] = [
-      //TODO add help sections that are relevant to this screen
+    const leftSections = [
+
+      // Move Draggable Items
+      new MoveDraggableItemsKeyboardHelpSection(),
+
+      // Slider Controls
+      new SliderControlsKeyboardHelpSection()
     ];
 
     // Sections in the right column.
     const rightSections = [
-
-      //TODO add help sections that are relevant to this screen
 
       // Basic Actions
       new BasicActionsKeyboardHelpSection( {
