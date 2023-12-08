c107b197de664530b532495bacf0dd07497e1722
pixelzoom <cmalley@pixelzoom.com>
2023-12-08 14:59:37 -0700

    polish FluxDisplayNode

--- js/common/view/FluxDisplayNode.ts
+++ js/common/view/FluxDisplayNode.ts
@@ -9,7 +9,7 @@
  */
 
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
-import { DragListener, RichText, Text, VBox } from '../../../../scenery/js/imports.js';
+import { DragListener, GridBox, RichText, RichTextOptions, Text, VBox } from '../../../../scenery/js/imports.js';
 import PickupCoil from '../model/PickupCoil.js';
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
@@ -19,11 +19,21 @@ import FELConstants from '../FELConstants.js';
 import Utils from '../../../../dot/js/Utils.js';
 
 const PHI = '\u03A6';
-const DELTA_PHI = `${PHI}\u2206`;
+const DELTA_PHI = `\u2206${PHI}`;
 const DECIMAL_PLACES = 3;
 
-const richTextOptions = {
-  font: new PhetFont( 12 )
+const labelTextOptions: RichTextOptions = {
+  font: new PhetFont( 12 ),
+  layoutOptions: {
+    xAlign: 'right'
+  }
+};
+
+const valueTextOptions: RichTextOptions = {
+  font: new PhetFont( 12 ),
+  layoutOptions: {
+    xAlign: 'left'
+  }
 };
 
 export default class FluxDisplayNode extends Panel {
@@ -31,35 +41,55 @@ export default class FluxDisplayNode extends Panel {
   public constructor( pickupCoil: PickupCoil ) {
 
     const averageBxStringProperty = new DerivedProperty( [ pickupCoil.averageBxProperty ],
-      averageBx => `Avg B<sub>x</sub> = ${Utils.toFixed( averageBx, DECIMAL_PLACES )} G` );
+      averageBx => `${Utils.toFixed( averageBx, DECIMAL_PLACES )} G` );
 
     const fluxStringProperty = new DerivedProperty( [ pickupCoil.fluxProperty ],
-      flux => `${PHI} = ${Utils.toFixed( flux, DECIMAL_PLACES )} Wb` );
+      flux => `${Utils.toFixed( flux, DECIMAL_PLACES )} Wb` );
 
     const deltaFluxStringProperty = new DerivedProperty( [ pickupCoil.deltaFluxProperty ],
-      deltaFlux => `${DELTA_PHI} = ${Utils.toFixed( deltaFlux, DECIMAL_PLACES )} Wb` );
+      deltaFlux => `${Utils.toFixed( deltaFlux, DECIMAL_PLACES )} Wb` );
 
     const emfStringProperty = new DerivedProperty( [ pickupCoil.emfProperty ],
-      emf => `EMF = ${Utils.toFixed( emf, DECIMAL_PLACES )} V` );
+      emf => `${Utils.toFixed( emf, DECIMAL_PLACES )} V` );
+
+    const titleText = new Text( 'Flux Display', {
+      font: FELConstants.TITLE_FONT
+    } );
+
+    const gridBox = new GridBox( {
+      spacing: 5,
+      columns: [
+        // Labels
+        [
+          new RichText( 'Avg B<sub>x</sub> =', labelTextOptions ),
+          new RichText( `${PHI} =`, labelTextOptions ),
+          new RichText( `${DELTA_PHI} =`, labelTextOptions ),
+          new RichText( 'EMF =', labelTextOptions )
+        ],
+
+        // Values
+        [
+          new RichText( averageBxStringProperty, valueTextOptions ),
+          new RichText( fluxStringProperty, valueTextOptions ),
+          new RichText( deltaFluxStringProperty, valueTextOptions ),
+          new RichText( emfStringProperty, valueTextOptions )
+        ]
+      ]
+    } );
 
     const content = new VBox( {
       align: 'left',
       spacing: 5,
       children: [
-        new Text( 'Flux Display', {
-          font: FELConstants.TITLE_FONT
-        } ),
-        new RichText( averageBxStringProperty, richTextOptions ),
-        new RichText( fluxStringProperty, richTextOptions ),
-        new RichText( deltaFluxStringProperty, richTextOptions ),
-        new RichText( emfStringProperty, richTextOptions )
+        titleText,
+        gridBox
       ]
     } );
 
     super( content, combineOptions<PanelOptions>( {}, FELConstants.PANEL_OPTIONS, {
       visibleProperty: pickupCoil.devFluxVisibleProperty,
       cursor: 'pointer',
-      preferredWidth: 150, // set empirically so that the panel does not resize
+      preferredWidth: 175, // set empirically so that the panel does not resize
       layoutOptions: {
         stretch: false
       }
