6497faa11d606f9f1493ce0b8f025880c2759f29
pixelzoom <cmalley@pixelzoom.com>
2023-12-15 11:02:20 -0700

    factor out BarNode

--- js/common/view/BarMagnetNode.ts
+++ js/common/view/BarMagnetNode.ts
@@ -22,6 +22,7 @@ import { Shape } from '../../../../kite/js/imports.js';
 import ShadedRectangle from '../../../../scenery-phet/js/ShadedRectangle.js';
 import Bounds2 from '../../../../dot/js/Bounds2.js';
 import FaradaysElectromagneticLabStrings from '../../FaradaysElectromagneticLabStrings.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
 
 const CORNER_RADIUS = 10;
 const FONT = new PhetFont( { size: 30, weight: 'bold' } );
@@ -36,58 +37,44 @@ export default class BarMagnetNode extends FELMovableNode {
 
   public constructor( barMagnet: BarMagnet, providedOptions: BarMagnetNodeOptions ) {
 
-    const southNode = new ShadedRectangle( new Bounds2( 0, 0, barMagnet.size.width, barMagnet.size.height ), {
-      baseColor: FELColors.barMagnetSouthColorProperty,
-      cornerRadius: CORNER_RADIUS
-    } );
+    // The bar, with north and south sections
+    const barNode = new BarNode( barMagnet.size );
 
-    const northNode = new ShadedRectangle( new Bounds2( 0, 0, barMagnet.size.width, barMagnet.size.height ), {
-      baseColor: FELColors.barMagnetNorthColorProperty,
-      clipArea: Shape.roundedRectangleWithRadii( barMagnet.size.width / 2, 0, barMagnet.size.width / 2, barMagnet.size.height, {
-        topLeft: 0,
-        topRight: CORNER_RADIUS,
-        bottomRight: CORNER_RADIUS,
-        bottomLeft: 0
-      } )
-    } );
-
-    const barNode = new Node( {
-      children: [ southNode, northNode ],
-      center: Vector2.ZERO
-    } );
-
-    const textXMargin = 0.06 * barMagnet.size.width;
-    const textMaxWidth = 0.25 * barMagnet.size.width;
-    const textMaxHeight = 0.85 * barMagnet.size.height;
-
+    // 'N' and 'S' labels for the poles of the magnet
     const textOptions = {
       font: FONT,
-      maxWidth: textMaxWidth,
-      maxHeight: textMaxHeight
+      maxWidth: 0.25 * barMagnet.size.width,
+      maxHeight: 0.85 * barMagnet.size.height
     };
-
-    const southText = new Text( FaradaysElectromagneticLabStrings.symbol.SStringProperty,
-      combineOptions<TextOptions>( {
-        fill: FELColors.barMagnetSouthTextColorProperty
-      }, textOptions ) );
-    southText.boundsProperty.link( () => {
-      southText.left = barNode.left + textXMargin;
-      southText.centerY = barNode.centerY;
-    } );
 
     const northText = new Text( FaradaysElectromagneticLabStrings.symbol.NStringProperty,
       combineOptions<TextOptions>( {
         fill: FELColors.barMagnetNorthTextColorProperty
       }, textOptions ) );
+
+    const southText = new Text( FaradaysElectromagneticLabStrings.symbol.SStringProperty,
+      combineOptions<TextOptions>( {
+        fill: FELColors.barMagnetSouthTextColorProperty
+      }, textOptions ) );
+
+
+    // Dynamically position the labels at the ends of the magnet.
+    const textXMargin = 0.06 * barMagnet.size.width;
+
     northText.boundsProperty.link( () => {
       northText.right = barNode.right - textXMargin;
       northText.centerY = barNode.centerY;
     } );
 
+    southText.boundsProperty.link( () => {
+      southText.left = barNode.left + textXMargin;
+      southText.centerY = barNode.centerY;
+    } );
+
     const options = optionize<BarMagnetNodeOptions, StrictOmit<SelfOptions, 'seeInsideProperty'>, FELMovableNodeOptions>()( {
 
       // FELMovableNodeOptions
-      children: [ barNode, southText, northText ]
+      children: [ barNode, northText, southText ]
     }, providedOptions );
 
     super( barMagnet, options );
@@ -104,6 +91,38 @@ export default class BarMagnetNode extends FELMovableNode {
         tandem: options.tandem.createTandem( 'fieldInsideNode' )
       } ) );
     }
+  }
+}
+
+/**
+ * BarNode is the bar for the bar magnet, with north and south sections.
+ */
+class BarNode extends Node {
+
+  public constructor( size: Dimension2 ) {
+
+    // The entire bar
+    const northNode = new ShadedRectangle( new Bounds2( 0, 0, size.width, size.height ), {
+      baseColor: FELColors.barMagnetNorthColorProperty,
+      cornerRadius: CORNER_RADIUS
+    } );
+
+    // The entire bar clipped to the south section
+    const southNode = new ShadedRectangle( new Bounds2( 0, 0, size.width, size.height ), {
+      baseColor: FELColors.barMagnetSouthColorProperty,
+      cornerRadius: CORNER_RADIUS,
+      clipArea: Shape.roundedRectangleWithRadii( 0, 0, size.width / 2, size.height, {
+        topLeft: CORNER_RADIUS,
+        topRight: 0,
+        bottomRight: 0,
+        bottomLeft: CORNER_RADIUS
+      } )
+    } );
+
+    super( {
+      children: [ northNode, southNode ], // clipped section on top!
+      center: Vector2.ZERO // origin at the center
+    } );
   }
 }
 
