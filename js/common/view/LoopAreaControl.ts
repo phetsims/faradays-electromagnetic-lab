30b6ec090252d50518dd6442c2a2efd3ddf540c6
pixelzoom <cmalley@pixelzoom.com>
2024-01-09 13:11:21 -0700

    use MappedProperty for handling "Loop Area" in percent

--- js/common/view/LoopAreaControl.ts
+++ js/common/view/LoopAreaControl.ts
@@ -1,7 +1,5 @@
 // Copyright 2023-2024, University of Colorado Boulder
 
-//TODO Use MappedProperty here to convert from loopAreaProperty to percent, for slider alt input?
-
 /**
  * LoopAreaControl controls the radius of all loops in a pickup coil. The control shows values in %.
  *
@@ -16,12 +14,15 @@ import FaradaysElectromagneticLabStrings from '../../FaradaysElectromagneticLabS
 import { combineOptions } from '../../../../phet-core/js/optionize.js';
 import FELConstants from '../FELConstants.js';
 import PatternStringProperty from '../../../../axon/js/PatternStringProperty.js';
+import Range from '../../../../dot/js/Range.js';
 import Utils from '../../../../dot/js/Utils.js';
 import { RichText } from '../../../../scenery/js/imports.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
+import MappedProperty from '../../../../axon/js/MappedProperty.js';
+import NumberIO from '../../../../tandem/js/types/NumberIO.js';
 
 const valuePercentStringProperty = FaradaysElectromagneticLabStrings.pattern.valuePercentStringProperty;
-const SLIDER_STEP = 1;
+const SLIDER_STEP = 1; // %
 
 export default class LoopAreaControl extends NumberControl {
 
@@ -29,21 +30,34 @@ export default class LoopAreaControl extends NumberControl {
 
     const loopAreaRange = loopAreaProperty.rangeProperty.value;
 
+    const minPercent = Utils.roundToInterval( 100 * loopAreaRange.min / loopAreaRange.max, 1 );
+    const maxPercent = 100;
+    const percentRange = new Range( minPercent, maxPercent );
+
+    const loopAreaPercentProperty = new MappedProperty<number, number>( loopAreaProperty, {
+      bidirectional: true,
+      map: ( loopArea: number ) => 100 * loopArea / loopAreaRange.max,
+      inverseMap: ( percent: number ) => percent * loopAreaRange.max / 100,
+      tandem: tandem.createTandem( 'loopAreaPercentProperty' ),
+      phetioValueType: NumberIO,
+      phetioReadOnly: true // use loopAreaProperty
+    } );
+
     const majorTicks = [
 
       // 20%
       {
-        value: loopAreaRange.min,
+        value: minPercent,
         label: new RichText( new PatternStringProperty( valuePercentStringProperty, {
-          value: Utils.roundToInterval( 100 * loopAreaRange.min / loopAreaRange.max, 1 )
+          value: minPercent
         } ), FELConstants.TICK_LABEL_OPTIONS )
       },
 
       // 100%
       {
-        value: loopAreaRange.max,
+        value: maxPercent,
         label: new RichText( new PatternStringProperty( valuePercentStringProperty, {
-          value: 100
+          value: maxPercent
         } ), FELConstants.TICK_LABEL_OPTIONS )
       }
     ];
@@ -53,23 +67,24 @@ export default class LoopAreaControl extends NumberControl {
       delta: SLIDER_STEP,
       numberDisplayOptions: {
         decimalPlaces: 0,
-        numberFormatter: loopArea => StringUtils.fillIn( valuePercentStringProperty, {
-          value: Utils.roundToInterval( 100 * loopArea / loopAreaRange.max, 1 )
+        numberFormatter: percent => StringUtils.fillIn( valuePercentStringProperty, {
+          value: percent
         } ),
         numberFormatterDependencies: [ valuePercentStringProperty ]
       },
       sliderOptions: {
         constrainValue: ( value: number ) => Utils.roundToInterval( value, SLIDER_STEP ),
-        majorTicks: majorTicks
-        //TODO alt input options
-        // keyboardStep: ?,
-        // shiftKeyboardStep: ?,
-        // pageKeyboardStep: ?
+        majorTicks: majorTicks,
+        keyboardStep: 5,
+        shiftKeyboardStep: 1,
+        pageKeyboardStep: 10
       },
       tandem: tandem
     } );
 
-    super( FaradaysElectromagneticLabStrings.loopAreaStringProperty, loopAreaProperty, loopAreaRange, options );
+    super( FaradaysElectromagneticLabStrings.loopAreaStringProperty, loopAreaPercentProperty, percentRange, options );
+
+    this.addLinkedElement( loopAreaProperty );
   }
 }
 
