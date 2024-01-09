e92e55025488b630e11e5faa46ccd92f7062d438
phet-dev <phetadmin@gmail.com>
2024-01-09 03:12:43 -0700

    update copyright dates from daily grunt work

--- js/common/model/SourceCoil.ts
+++ js/common/model/SourceCoil.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * SourceCoil is the model of the source coil for the electromagnet.
--- js/common/view/PickupCoilPanel.ts
+++ js/common/view/PickupCoilPanel.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * PickupCoilPanel is the panel that contains controls for changing properties of the pickup coil.
