e7a14cab64c6a0ab6c5b7af1f1831a07e8300779
pixelzoom <cmalley@pixelzoom.com>
2024-01-15 16:50:05 -0700

    doc

--- js/common/view/FELMovableNode.ts
+++ js/common/view/FELMovableNode.ts
@@ -48,6 +48,7 @@ export default class FELMovableNode extends Node {
       phetioVisiblePropertyInstrumented: false
     }, providedOptions );
 
+    // Add options related to input listeners.
     if ( options.isMovable ) {
       options.cursor = 'pointer';
       options.phetioInputEnabledPropertyInstrumented = true;
