eff6583a623460ae0798599c4a8806b8a374ff9b
phet-dev <phetadmin@gmail.com>
2024-01-16 03:12:27 -0700

    update copyright dates from daily grunt work

--- js/common/model/FELMovable.ts
+++ js/common/model/FELMovable.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * FELMovable is the base class for model elements that have a mutable position.
