2cf83dd152bf43067ad2553b0da72975c0503065
phet-dev <phetadmin@gmail.com>
2024-01-18 03:12:13 -0700

    update copyright dates from daily grunt work

--- js/generator/model/Turbine.ts
+++ js/generator/model/Turbine.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * Turbine is the model of a simple turbine generator, where water flow rotates a bar magnet, causing the magnetic
