0e26d13c08092b4fa9b4699566118c755ac0849a
pixelzoom <cmalley@pixelzoom.com>
2024-01-23 15:13:41 -0700

    use pickup coil's initial y value as the "Lock to Axis" location, https://github.com/phetsims/faradays-electromagnetic-lab/issues/25

--- js/transformer/view/TransformerScreenView.ts
+++ js/transformer/view/TransformerScreenView.ts
@@ -113,7 +113,7 @@ export default class TransformerScreenView extends FELScreenView {
         if ( isLockedToAxis ) {
 
           // Move the pickup coil and magnet to a good position for horizontal dragging.
-          const y = 400;
+          const y = model.pickupCoil.positionProperty.initialValue.y;
           model.pickupCoil.positionProperty.value = new Vector2( model.pickupCoil.positionProperty.value.x, y );
           model.electromagnet.positionProperty.value = new Vector2( model.electromagnet.positionProperty.value.x, y );
 
