54b02d1d9b3572723669fe1d75f522f1179834b4
phet-dev <phetadmin@gmail.com>
2024-02-03 03:18:00 -0700

    update copyright dates from daily grunt work

--- js/generator/view/RPMDisplay.ts
+++ js/generator/view/RPMDisplay.ts
@@ -1,4 +1,4 @@
-// Copyright 2023, University of Colorado Boulder
+// Copyright 2023-2024, University of Colorado Boulder
 
 /**
  * RPMDisplay is the display of RPM (rotations per minute) that appears on the turbine generator.
