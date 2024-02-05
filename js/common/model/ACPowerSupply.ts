7ab7224bd7dcfc651219a7bc87c91cbf6e959051
pixelzoom <cmalley@pixelzoom.com>
2024-02-05 10:46:49 -0700

    deltaAngleProperty is not necessary, https://github.com/phetsims/faradays-electromagnetic-lab/issues/58

--- js/common/model/ACPowerSupply.ts
+++ js/common/model/ACPowerSupply.ts
@@ -38,9 +38,6 @@ export default class ACPowerSupply extends CurrentSource {
   // The current angle of the sine wave that describes the AC (in radians)
   private readonly angleProperty: NumberProperty;
 
-  // The change in angle at the current frequency (in radians)
-  private readonly deltaAngleProperty: TReadOnlyProperty<number>;
-
   // The change in angle that occurred the last time stepInTime was called (in radians)
   public readonly stepAngleProperty: TReadOnlyProperty<number>;
   private readonly _stepAngleProperty: NumberProperty;
@@ -94,16 +91,8 @@ export default class ACPowerSupply extends CurrentSource {
       phetioHighFrequency: true
     } );
 
-    this.deltaAngleProperty = new DerivedProperty( [ this.frequencyProperty ],
-      frequencyPercent => ( 2 * Math.PI * frequencyPercent / 100 ) / MIN_STEPS_PER_CYCLE, {
-        units: 'radians',
-        tandem: tandem.createTandem( 'deltaAngleProperty' ),
-        phetioValueType: NumberIO,
-        phetioDocumentation: 'For internal use only.'
-      } );
-
-    // Reset angle when delta angle is changed.
-    this.deltaAngleProperty.link( () => {
+    // Reset angle when frequency is changed.
+    this.frequencyProperty.link( () => {
       this.angleProperty.value = 0;
     } );
 
@@ -134,10 +123,11 @@ export default class ACPowerSupply extends CurrentSource {
       this.voltageProperty.value = 0;
     }
     else {
+      const deltaAngle = ( 2 * Math.PI * this.frequencyProperty.value / 100 ) / MIN_STEPS_PER_CYCLE;
       const previousAngle = this.angleProperty.value;
 
       // Compute the next angle.
-      const nextAngle = previousAngle + ( dt * this.deltaAngleProperty.value );
+      const nextAngle = previousAngle + deltaAngle;
 
       // The change in angle on this tick of the simulation clock.
       this._stepAngleProperty.value = nextAngle - previousAngle;
