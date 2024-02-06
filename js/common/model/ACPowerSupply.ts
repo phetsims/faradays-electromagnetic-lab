f68855ba9a81e81e30cfdaad26b7d124fbf4d086
pixelzoom <cmalley@pixelzoom.com>
2024-02-05 19:28:50 -0700

    TODO, doc

--- js/common/model/ACPowerSupply.ts
+++ js/common/model/ACPowerSupply.ts
@@ -87,6 +87,7 @@ export default class ACPowerSupply extends CurrentSource {
       phetioDocumentation: 'Relative frequency of the change in voltage over time.'
     } );
 
+    //TODO https://github.com/phetsims/faradays-electromagnetic-lab/issues/59 Should numberOfCyclesProperty be an integer?
     this.numberOfCyclesProperty = new DerivedProperty( [ this.frequencyProperty ],
       frequency => ACPowerSupply.MAX_CYCLES * frequency / 100, {
         isValidValue: value => value >= 1 && value <= ACPowerSupply.MAX_CYCLES
@@ -120,9 +121,14 @@ export default class ACPowerSupply extends CurrentSource {
   public step( dt: number ): void {
     assert && assert( dt === ConstantDtClock.CONSTANT_DT, `invalid dt=${dt}, see ConstantStepEmitter` );
 
+    // Change in angle is a function of relative frequency.
     const deltaAngle = dt * ( this.frequencyProperty.value / 100 ) * MAX_DELTA_ANGLE;
+
+    // The angle must range over complete cycles, hence Math.ceil.
     const maxAngle = 2 * Math.PI * Math.ceil( this.numberOfCyclesProperty.value );
     this._angleProperty.value = ( this._angleProperty.value + deltaAngle ) % maxAngle;
+
+    // Voltage varies with the sine of the angle.
     this.voltageProperty.value = this.maxVoltageProperty.value * Math.sin( this._angleProperty.value );
   }
 }
