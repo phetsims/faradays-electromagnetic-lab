223a4e8822b80b4fb3629125b7ceece75485612a
pixelzoom <cmalley@pixelzoom.com>
2024-02-14 16:40:59 -0700

    add fade out, https://github.com/phetsims/faradays-electromagnetic-lab/issues/77

--- js/common/view/FieldMeterSoundListener.ts
+++ js/common/view/FieldMeterSoundListener.ts
@@ -9,7 +9,7 @@
 
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
 import { PressListenerEvent, TInputListener } from '../../../../scenery/js/imports.js';
-import { Disposable, TReadOnlyProperty } from '../../../../axon/js/imports.js';
+import { Disposable, stepTimer, TimerListener, TReadOnlyProperty } from '../../../../axon/js/imports.js';
 import Range from '../../../../dot/js/Range.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import soundManager from '../../../../tambo/js/soundManager.js';
@@ -19,25 +19,35 @@ import Utils from '../../../../dot/js/Utils.js';
 import FieldNode from './FieldNode.js';
 
 const OUTPUT_LEVEL = 0.7;
+
 const PLAYBACK_RATE_RANGE = new Range( 1, 2 ); // 1 octave
+
+// Fade out parameters
+const FADE_OUT_INTERVAL = 500; // time over which fade out occurs, in ms
+const FADE_OUTPUT_LEVEL_DELTA = 0.1; // change in output level per step
+const FADE_OUT_STEPS = PLAYBACK_RATE_RANGE.getLength() / FADE_OUTPUT_LEVEL_DELTA; // steps to complete the fade out
 
 export default class FieldMeterSoundListener implements TInputListener {
 
+  private readonly soundClip: SoundClip;
+
   private readonly fieldVectorProperty: TReadOnlyProperty<Vector2>;
   private readonly fieldMagnitudeRange: Range;
-  private readonly soundClip: SoundClip;
   private readonly fieldVectorListener: ( fieldVector: Vector2 ) => void;
 
-  public constructor( fieldVectorProperty: TReadOnlyProperty<Vector2>, fieldMagnitudeRange: Range, fieldScaleProperty: TReadOnlyProperty<number> ) {
+  private stepTimerListener: TimerListener | null;
+  private readonly fadeOutAndStopCallback: () => void;
 
-    this.fieldVectorProperty = fieldVectorProperty;
-    this.fieldMagnitudeRange = fieldMagnitudeRange;
+  public constructor( fieldVectorProperty: TReadOnlyProperty<Vector2>, fieldMagnitudeRange: Range, fieldScaleProperty: TReadOnlyProperty<number> ) {
 
     this.soundClip = new SoundClip( felFieldMeterLoop_mp3, {
       initialOutputLevel: OUTPUT_LEVEL,
       loop: true
     } );
     soundManager.addSoundGenerator( this.soundClip );
+
+    this.fieldVectorProperty = fieldVectorProperty;
+    this.fieldMagnitudeRange = fieldMagnitudeRange;
 
     this.fieldVectorListener = fieldVector => {
       if ( fieldVector.magnitude === 0 ) {
@@ -47,6 +57,18 @@ export default class FieldMeterSoundListener implements TInputListener {
         const playbackRate = this.fieldMagnitudeToPlaybackRate( fieldVector.magnitude, fieldScaleProperty.value );
         this.soundClip.setPlaybackRate( playbackRate );
         this.soundClip.setOutputLevel( OUTPUT_LEVEL );
+      }
+    };
+
+    this.stepTimerListener = null;
+
+    this.fadeOutAndStopCallback = () => {
+      assert && assert( this.stepTimerListener, 'expected stepTimerListener to be set' );
+      const outputLevel = Math.max( 0, this.soundClip.outputLevel - FADE_OUTPUT_LEVEL_DELTA );
+      this.soundClip.setOutputLevel( outputLevel );
+      if ( outputLevel === 0 ) {
+        this.soundClip.stop();
+        stepTimer.clearInterval( this.stepTimerListener! );
       }
     };
   }
@@ -74,13 +96,15 @@ export default class FieldMeterSoundListener implements TInputListener {
   private startSound(): void {
     phet.log && phet.log( 'FieldMeterSoundListener startSound' );
     this.fieldVectorProperty.link( this.fieldVectorListener );
+    this.stepTimerListener && stepTimer.clearInterval( this.stepTimerListener ); // Clear the fade that was in progress.
+    this.soundClip.setOutputLevel( OUTPUT_LEVEL );
     this.soundClip.play();
   }
 
   private stopSound(): void {
     phet.log && phet.log( 'FieldMeterSoundListener stopSound' );
     this.fieldVectorProperty.unlink( this.fieldVectorListener );
-    this.soundClip.stop();
+    this.stepTimerListener = stepTimer.setInterval( this.fadeOutAndStopCallback, FADE_OUT_INTERVAL / FADE_OUT_STEPS );
   }
 
   /**
