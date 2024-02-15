a6b2b75c3b5e8d9415982b773b628b43e0a0460c
pixelzoom <cmalley@pixelzoom.com>
2024-02-15 12:34:59 -0700

    doc

--- js/common/view/PickupCoilDebuggerPanel.ts
+++ js/common/view/PickupCoilDebuggerPanel.ts
@@ -3,7 +3,7 @@
 /**
  * PickupCoilDebuggerPanel is a movable panel that displays quantities associated with a PickupCoil. This is intended to
  * be used solely for development and debugging. It is not intended to be displayed in the production version. It does
- * not support switching between gauss and tesla, alternative input, and translated strings.
+ * not support localization, alternative input, or switching between gauss and Tesla.
  *
  * This is based on FluxDisplayGraphic.java in the Java version of this sim.
  *
