2deddcd5b9377e95ab172f4b0f5056723fead9c2
pixelzoom <cmalley@pixelzoom.com>
2024-02-19 10:07:38 -0700

    add units to electromagnet.strengthProperty, https://github.com/phetsims/faradays-electromagnetic-lab/issues/62

--- js/common/model/Electromagnet.ts
+++ js/common/model/Electromagnet.ts
@@ -86,6 +86,7 @@ export default class Electromagnet extends CoilMagnet {
     // Strength of the magnet is proportional to its EMF.
     const strengthProperty = new DerivedProperty( [ coil.currentAmplitudeProperty ],
       currentAmplitude => Math.abs( currentAmplitude ) * STRENGTH_RANGE.max, {
+        units: 'G',
         isValidValue: strength => STRENGTH_RANGE.contains( strength ),
         tandem: options.tandem.createTandem( 'strengthProperty' ),
         phetioValueType: NumberIO
