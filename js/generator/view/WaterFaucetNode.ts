1e5aaa4822b45cc02be1de57d9dc0daa5b37a73d
pixelzoom <cmalley@pixelzoom.com>
2024-03-05 15:22:23 -0700

    add temporary sound for faucet, https://github.com/phetsims/faradays-electromagnetic-lab/issues/90, https://github.com/phetsims/scenery-phet/issues/840

--- js/generator/view/WaterFaucetNode.ts
+++ js/generator/view/WaterFaucetNode.ts
@@ -1,5 +1,6 @@
 // Copyright 2024, University of Colorado Boulder
 
+//TODO https://github.com/phetsims/scenery-phet/issues/840 When FaucetNode sound is completed, delete code identified by "TEMPORARY SOUND".
 /**
  * WaterFaucetNode is the water faucet used to power the generator.
  *
@@ -10,9 +11,10 @@ import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
 import FaucetNode, { FaucetNodeOptions } from '../../../../scenery-phet/js/FaucetNode.js';
 import Property from '../../../../axon/js/Property.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
-import { NodeTranslationOptions } from '../../../../scenery/js/imports.js';
+import { DragListener, NodeTranslationOptions } from '../../../../scenery/js/imports.js';
 import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import WaterFaucet from '../model/WaterFaucet.js';
+import ValueChangeSoundPlayer from '../../../../tambo/js/sound-generators/ValueChangeSoundPlayer.js';
 
 type SelfOptions = EmptySelfOptions;
 
@@ -36,7 +38,28 @@ export default class WaterFaucetNode extends FaucetNode {
       phetioVisiblePropertyInstrumented: false
     }, providedOptions );
 
+    // TEMPORARY SOUND
+    const soundGenerator = new ValueChangeSoundPlayer( waterFaucet.flowRateProperty.range );
+
+    // TEMPORARY SOUND for keyboard drag. It's unclear why options.drag does not also apply to mouse/touch drag.
+    let previousValue = waterFaucet.flowRateProperty.value;
+    options.drag = event => {
+      if ( event.isFromPDOM() ) {
+        soundGenerator.playSoundForValueChange( waterFaucet.flowRateProperty.value, previousValue );
+      }
+      else {
+        soundGenerator.playSoundIfThresholdReached( waterFaucet.flowRateProperty.value, previousValue );
+      }
+      previousValue = waterFaucet.flowRateProperty.value;
+    };
+
     super( waterFaucet.flowRateProperty.range.max, waterFaucet.flowRateProperty, new Property( true ), options );
+
+    // TEMPORARY SOUND mouse/touch drag. It's unclear why options.drag does not also handle this.
+    this.addInputListener( new DragListener( {
+      attach: false,
+      drag: ( event, listener ) => options.drag( event )
+    } ) );
   }
 }
 
