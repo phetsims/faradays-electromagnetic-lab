ee77cb4d646af56c7cce75a79dfc1dff250a7700
pixelzoom <cmalley@pixelzoom.com>
2024-03-13 17:19:04 -0600

    factor out FELSim, https://github.com/phetsims/faradays-electromagnetic-lab/issues/101

---
+++ js/common/FELSim.ts
@@ -0,0 +1,52 @@
+// Copyright 2024, University of Colorado Boulder
+
+/**
+ * FELSim is the subclass of Sim used by all sims in the faradays-electromagnetic-lab family.
+ *
+ * @author Chris Malley (PixelZoom, Inc.)
+ */
+
+import faradaysElectromagneticLab from '../faradaysElectromagneticLab.js';
+import Sim from '../../../joist/js/Sim.js';
+import TReadOnlyProperty from '../../../axon/js/TReadOnlyProperty.js';
+import FELConstants from './FELConstants.js';
+import PreferencesModel from '../../../joist/js/preferences/PreferencesModel.js';
+import FELPreferencesNode from './view/preferences/FELPreferencesNode.js';
+import BarMagnetScreen from '../bar-magnet/BarMagnetScreen.js';
+import PickupCoilScreen from '../pickup-coil/PickupCoilScreen.js';
+import ElectromagnetScreen from '../electromagnet/ElectromagnetScreen.js';
+import TransformerScreen from '../transformer/TransformerScreen.js';
+import GeneratorScreen from '../generator/GeneratorScreen.js';
+
+type FELScreen = BarMagnetScreen | PickupCoilScreen | ElectromagnetScreen | TransformerScreen | GeneratorScreen;
+
+export default class FELSim extends Sim {
+
+  /**
+   * @param titleStringProperty
+   * @param screens
+   * @param hasEarthFeature - Whether the sim has the feature that shows the alignment of a bar magnet with planet Earth.
+   */
+  public constructor( titleStringProperty: TReadOnlyProperty<string>, screens: FELScreen[], hasEarthFeature = true ) {
+
+    super( titleStringProperty, screens, {
+      credits: FELConstants.CREDITS,
+      phetioDesigned: true,
+      preferencesModel: new PreferencesModel( {
+        visualOptions: {
+          supportsProjectorMode: true
+        },
+        simulationOptions: {
+          customPreferences: [ {
+            createContent: tandem => new FELPreferencesNode( {
+              hasEarthFeature: hasEarthFeature,
+              tandem: tandem.createTandem( 'simPreferences' )
+            } )
+          } ]
+        }
+      } )
+    } );
+  }
+}
+
+faradaysElectromagneticLab.register( 'FELSim', FELSim );
--- js/faradays-electromagnetic-lab-main.ts
+++ js/faradays-electromagnetic-lab-main.ts
@@ -6,7 +6,6 @@
  * @author Chris Malley (PixelZoom, Inc.)
  */
 
-import Sim, { SimOptions } from '../../joist/js/Sim.js';
 import simLauncher from '../../joist/js/simLauncher.js';
 import Tandem from '../../tandem/js/Tandem.js';
 import PickupCoilScreen from './pickup-coil/PickupCoilScreen.js';
@@ -16,14 +15,10 @@ import BarMagnetScreen from './bar-magnet/BarMagnetScreen.js';
 import ElectromagnetScreen from './electromagnet/ElectromagnetScreen.js';
 import TransformerScreen from './transformer/TransformerScreen.js';
 import GeneratorScreen from './generator/GeneratorScreen.js';
-import PreferencesModel from '../../joist/js/preferences/PreferencesModel.js';
-import FELConstants from './common/FELConstants.js';
-import FELPreferencesNode from './common/view/preferences/FELPreferencesNode.js';
+import FELSim from './common/FELSim.js';
 
 simLauncher.launch( () => {
-
   const titleStringProperty = FaradaysElectromagneticLabStrings[ 'faradays-electromagnetic-lab' ].titleStringProperty;
-
   const screens = [
     new BarMagnetScreen( Tandem.ROOT.createTandem( 'barMagnetScreen' ) ),
     new PickupCoilScreen( Tandem.ROOT.createTandem( 'pickupCoilScreen' ) ),
@@ -31,24 +26,6 @@ simLauncher.launch( () => {
     new TransformerScreen( Tandem.ROOT.createTandem( 'transformerScreen' ) ),
     new GeneratorScreen( Tandem.ROOT.createTandem( 'generatorScreen' ) )
   ];
-
-  const options: SimOptions = {
-    credits: FELConstants.CREDITS,
-    phetioDesigned: true,
-    preferencesModel: new PreferencesModel( {
-      visualOptions: {
-        supportsProjectorMode: true
-      },
-      simulationOptions: {
-        customPreferences: [ {
-          createContent: tandem => new FELPreferencesNode( {
-            tandem: tandem.createTandem( 'simPreferences' )
-          } )
-        } ]
-      }
-    } )
-  };
-
-  const sim = new Sim( titleStringProperty, screens, options );
+  const sim = new FELSim( titleStringProperty, screens );
   sim.start();
 } );
