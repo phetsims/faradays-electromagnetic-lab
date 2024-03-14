5dbb09c5f97d35a0fd6c37616a5f3c7eb8402cb6
pixelzoom <cmalley@pixelzoom.com>
2024-03-14 17:52:08 -0600

    make voltmeter needle more incrementally to zero, with deceleration, https://github.com/phetsims/faradays-electromagnetic-lab/issues/73

--- js/common/model/Voltmeter.ts
+++ js/common/model/Voltmeter.ts
@@ -21,24 +21,12 @@ import PickRequired from '../../../../phet-core/js/types/PickRequired.js';
 import BooleanProperty from '../../../../axon/js/BooleanProperty.js';
 import optionize from '../../../../phet-core/js/optionize.js';
 
-// Define the zero point of the needle.
-const ZERO_NEEDLE_ANGLE = Utils.toRadians( 0 );
+// When the angle is this close to zero, move immediately to zero.
+const ZERO_THRESHOLD = Utils.toRadians( 0.5 );
 
-// The needle deflection range is this much on either side of the zero point.
-const MAX_NEEDLE_ANGLE = Utils.toRadians( 90 );
-
-// If rotational kinematics is enabled, the needle will jiggle this much around the zero reading.
-const NEEDLE_JIGGLE_ANGLE = Utils.toRadians( 3 );
-
-// When the angle is this close to zero, the needle stops jiggling.
-const NEEDLE_JIGGLE_THRESHOLD = Utils.toRadians( 0.5 );
-
-// Determines how much the needle jiggles around the zero point.
-// The value L should be such that 0 < L < 1.
-// If set to 0, the needle will not jiggle at all.
-// If set to 1, the needle will oscillate forever.
-const NEEDLE_LIVELINESS = 0.6;
-assert && assert( NEEDLE_LIVELINESS > 0 && NEEDLE_LIVELINESS < 1 );
+// Damping as the needle moves back towards zero. Decrease this to make the needle move more slowly.
+const NEEDLE_DAMPING = 0.24;
+assert && assert( NEEDLE_DAMPING > 0 && NEEDLE_DAMPING <= 1 );
 
 // Kinematics of the needle are enabled by default.
 const DEFAULT_KINEMATICS_ENABLED_PROPERTY: TReadOnlyProperty<boolean> = new BooleanProperty( true );
@@ -78,7 +66,7 @@ export default class Voltmeter extends CurrentIndicator {
     this.currentAmplitudeProperty = currentAmplitudeProperty;
     this.currentAmplitudeRange = currentAmplitudeRange;
 
-    this.needAngleRange = new Range( -MAX_NEEDLE_ANGLE, MAX_NEEDLE_ANGLE );
+    this.needAngleRange = new Range( -Math.PI / 2, Math.PI / 2 );
     this._needleAngleProperty = new NumberProperty( 0, {
       units: 'radians',
       range: this.needAngleRange,
@@ -104,22 +92,20 @@ export default class Voltmeter extends CurrentIndicator {
     const deltaAngle = ( desiredNeedleAngle - this._needleAngleProperty.value ) % ( 2 * Math.PI );
 
     if ( deltaAngle !== 0 ) {
-      if ( !this.kinematicsEnabledProperty.value || desiredNeedleAngle !== ZERO_NEEDLE_ANGLE ) {
+      if ( !this.kinematicsEnabledProperty.value || Math.abs( desiredNeedleAngle ) > Math.abs( this._needleAngleProperty.value ) ) {
 
-        // Move immediately to the desired angle.
+        // If kinematics is disabled, or the desired angle is larger, move immediately to the desired angle.
         this._needleAngleProperty.value = desiredNeedleAngle;
       }
-      else if ( Math.abs( deltaAngle ) < NEEDLE_JIGGLE_THRESHOLD ) {
+      else if ( Math.abs( deltaAngle ) < ZERO_THRESHOLD ) {
 
-        // The needle is close enough to zero, so stop jiggling.
-        this._needleAngleProperty.value = ZERO_NEEDLE_ANGLE;
+        // The needle is close enough to zero, so move immediately to zero.
+        this._needleAngleProperty.value = 0;
       }
       else {
 
-        // Jiggle the needle around the zero point.
-        let jiggleAngle = -deltaAngle * NEEDLE_LIVELINESS;
-        jiggleAngle = Utils.clamp( jiggleAngle, -NEEDLE_JIGGLE_ANGLE, NEEDLE_JIGGLE_ANGLE );
-        this._needleAngleProperty.value = jiggleAngle;
+        // Move incrementally towards zero, decelerating as we get closer to zero.
+        this._needleAngleProperty.value += ( NEEDLE_DAMPING * deltaAngle );
       }
     }
   }
