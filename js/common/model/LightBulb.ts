853575d24a7d95d1da93f2ac3460e6300f1325b8
pixelzoom <cmalley@pixelzoom.com>
2024-03-29 14:02:10 -0600

    TODOs for https://github.com/phetsims/faradays-electromagnetic-lab/issues/152

--- js/common/model/LightBulb.ts
+++ js/common/model/LightBulb.ts
@@ -26,8 +26,7 @@ type SelfOptions = {
   // Determines whether the bulb lights when the current in the coil changes direction.
   // In some cases (e.g. flipping the bar magnet) this should be true.
   // In other cases (eg, the Generator or AC Electromagnet) this should be false.
-
-  // REVIEW - Can you explain more about the difference between these two cases? Why is this needed, and why does the designed behavior require this distinction?
+  //TODO https://github.com/phetsims/faradays-electromagnetic-lab/issues/152 Improve documentation of this feature, its use for sinusoidal sources, why that's necessary.
   lightsWhenCurrentChangesDirection?: boolean;
 };
 
