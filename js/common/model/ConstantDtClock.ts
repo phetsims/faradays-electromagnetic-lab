bf06801fb1ae351093bf201d09ab791eabeb1aaf
pixelzoom <cmalley@pixelzoom.com>
2024-06-20 12:13:30 -0600

    revert to original implementation of ConstantDtClock, which does not use EventTimer, https://github.com/phetsims/faradays-electromagnetic-lab/issues/180

--- js/common/model/ConstantDtClock.ts
+++ js/common/model/ConstantDtClock.ts
@@ -1,13 +1,12 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ConstantDtClock implements a clock that 'ticks' at a constant rate, with a constant dt.
- * In the Java version of this sim, we used a clock that fires 25 times per second, with constant dt = 1.
- * In FaradayModule.java, see new SwingClock( 1000 / 25, 1 ).
- * Because so much of the code ported from Java relies on this, we implement something similar here.
+ * ConstantDtClock implements a clock that steps at a constant rate, with a constant dt. We are not using EventTimer
+ * here because EventTimer will result in multiple steps if dt is large, and that is undesirable for this sim.
  *
- * Note that this is an extension of EventTimer, with the ability to add listeners that are notified when
- * the event occurs. In EventTimer terminology, the event is a 'clock tick'.
+ * In the Java version of this sim, we used a clock that steps 25 times per second, with constant dt = 1.
+ * See FaradayModule.java: new SwingClock( 1000 / 25, 1 )
+ * Because so much of the code ported from Java relies on this, we implement something similar here.
  *
  * @author Chris Malley (PixelZoom, Inc.)
  */
@@ -15,34 +14,29 @@
 import Emitter from '../../../../axon/js/Emitter.js';
 import faradaysElectromagneticLab from '../../faradaysElectromagneticLab.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
-import EventTimer, { ConstantEventModel } from '../../../../phet-core/js/EventTimer.js';
 
-export default class ConstantDtClock extends EventTimer {
+export default class ConstantDtClock {
 
   // Constant framerate, inherited from the Java implementation. Increasing this value makes the sim run faster,
   // reducing it makes the sim run slower. Changing this value may have unintended/unknown consequences, so proceed
   // with caution.
   public static readonly FRAMES_PER_SECOND = 25;
+  private static readonly SECONDS_PER_FRAME = 1 / ConstantDtClock.FRAMES_PER_SECOND;
 
-  // Constant dt, inherited from the Java implementation. Each time the clock ticks (about every 40 ms at 25 fps),
-  // we'll step with this dt value. So while step is usually called with a value in seconds, this value is not in
-  // seconds. Changing this value may have unintended/unknown consequences, so proceed with caution.
+  // Constant dt, inherited from the Java implementation. Each time the clock steps (about every 40 ms at 25 fps),
+  // we'll notify listeners with this dt value. So while step is usually called with a value in seconds, this value
+  // is not in seconds. Changing this value may have unintended/unknown consequences, so proceed with caution.
   public static readonly DT = 1;
 
-  // For notifying listeners when the clock ticks. The single parameter to emit is dt.
+  // Time since the clock last notified listeners.
+  private elapsedTime: number;
+
+  // For notifying listeners when the clock steps. The single parameter to emit is the constant dt value.
   private readonly emitter: Emitter<[ number ]>;
 
   public constructor() {
 
-    // Notify listeners that the clock has ticked, with a constant dt.
-    // EventTimer's timeElapsed is how long ago the event *should* have occurred, and we don't need that for this sim.
-    // Calling EventTimer step with a large dt will result in multiple calls to eventCallback (multiple clock ticks).
-    const eventCallback = ( timeElapsed: number ) => this.emitter.emit( ConstantDtClock.DT );
-
-    // eventCallback will be called every 1 / FRAMES_PER_SECOND seconds.
-    const eventModel = new ConstantEventModel( ConstantDtClock.FRAMES_PER_SECOND );
-
-    super( eventModel, eventCallback );
+    this.elapsedTime = 0;
 
     this.emitter = new Emitter( {
       parameters: [ { name: 'dt', phetioType: NumberIO } ]
@@ -58,7 +52,28 @@ export default class ConstantDtClock extends EventTimer {
   }
 
   /**
-   * Advances the model by one constant-dt step. Used by the step button in time controls.
+   * Accumulates dt until it reaches the constant dt, then emits once.
+   * Unlike EventTimer, this will not result in more than 1 step if dt is large.
+   *
+   * @param dt - time change, in seconds
+   */
+  public step( dt: number ): void {
+    this.elapsedTime += dt;
+
+    // When we've accumulated enough time to step ...
+    if ( this.elapsedTime >= ConstantDtClock.SECONDS_PER_FRAME ) {
+
+      // Advance one step.
+      this.stepOnce();
+
+      // Apply the remainder to the next step.
+      this.elapsedTime %= ConstantDtClock.SECONDS_PER_FRAME;
+    }
+  }
+
+  /**
+   * Advances the model by one step, with constant dt. In addition to being called by the step method, this is called
+   * explicitly by the step button in time controls.
    */
   public stepOnce(): void {
     this.emitter.emit( ConstantDtClock.DT );
