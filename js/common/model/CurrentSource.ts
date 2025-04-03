7d1bebe6270309dd6b38162218973a1494f32f8a
phet-dev <phetadmin@gmail.com>
2025-04-03 03:19:33 -0600

    update copyright dates from daily grunt work

--- js/common/model/CurrentSource.ts
+++ js/common/model/CurrentSource.ts
@@ -1,4 +1,4 @@
-// Copyright 2023-2024, University of Colorado Boulder
+// Copyright 2023-2025, University of Colorado Boulder
 
 /**
  * CurrentSource is the base class for all devices that are capable of acting as a source of electrical current
